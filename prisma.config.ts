309d87e370ce69e83c42dfcc41a55ff41098efea
knzth <khun.noonzayar@gmail.com>
2026-08-25 00:24:37 +0630

    remove seeder

--- prisma.config.ts
+++ prisma.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
-    seed: "tsx src/database/seed.ts",
   },
   datasource: {
     url: process.env.DATABASE_URL,
